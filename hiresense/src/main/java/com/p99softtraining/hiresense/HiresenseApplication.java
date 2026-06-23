920f045f31b579d34fcb34dbd09504c9b73fa1d3
Ritesh Ukade P99SOFT <ritesh.ukade@p99soft.com>
2026-06-23 22:35:12 +0530

    Git shifted to root folder

---
+++ hiresense/src/main/java/com/p99softtraining/hiresense/HiresenseApplication.java
@@ -0,0 +1,13 @@
+package com.p99softtraining.hiresense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiresenseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiresenseApplication.class, args);
+	}
+
+}
